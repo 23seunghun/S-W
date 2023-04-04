28201609ef23d5f067ee0c292189716caf97c427
승훈 <127714089+23seunghun@users.noreply.github.com>
2023-04-04 13:18:48 +0900

    Add files via upload

---
+++ 0328/p115_6.py
@@ -0,0 +1,10 @@
+su1=47 #num1 변수에 정수 47을 저장
+su2=12 #num1 변수에 정수 18을 저장
+
+  
+
+print(su1,"과",su1,"의 합은:",su1+su2)
+print(su1,"과",su1,"의 차는:",su1-su2)
+print(su1,"과",su1,"의 곱은:",su1*su2)
+print(su1,"과",su1,"의 나눗셈은:",su1/su2)
+
