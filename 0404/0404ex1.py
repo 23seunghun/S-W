a1b46226f1d309ec5a2350d95a776ae9595bff76
승훈 <127714089+23seunghun@users.noreply.github.com>
2023-04-04 13:16:54 +0900

    Add files via upload

---
+++ 0404/0404ex1.py
@@ -0,0 +1,17 @@
+"""
+2023.04.04 신승훈 표준출력 프린트 연습
+
+"""
+
+print('한국','korea',2023)
+print('한국','korea',2023,sep='-') #구분자(-)넣어서 출력
+print('한국','korea',2023,end='@') #문장끝 (@)출력 
+print() #한 줄 띄우기
+print('pyhon','language');print('Hi pyhon')
+print('pyhon','language',end='');print('Hi pyhon')
+print('HI pyhon')
+print("'Hi pyhon'") #작은 따옴표 출력
+print("\'Hi pyhon\'") #작은 따옴표 출력
+print("phihon "*5) #5번 반복 출력
+print("hi \n phthon") #\n (줄바꿈) 출력
+print("\n 포멧 활용") #한줄 띄우고 문자열 출력
---
+++ 0404/0404ex2.py
@@ -0,0 +1,9 @@
+"""
+2023.04.04 신승훈
+표준 출력 (print) format()연습
+"""
+
+print('이름은 {}이고 나이는 {}입니다.'.format('ssh',20))
+print('이름 {name}, 나이{age}, 주소{add}'.format(add='Busan',name='ssh',age='20'))
+print('The Iucky number is ({:14})'.format(7777)) #숫자 기본 오른쪽 정렬
+print('The Iucky number is ({:<14})'.format(7777))
