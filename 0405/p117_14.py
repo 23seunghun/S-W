af32d6753f4fa8949824a8e8eab18034ddf3841e
승훈 <127714089+23seunghun@users.noreply.github.com>
2023-04-05 14:19:14 +0900

    Add files via upload

---
+++ 0405/p117_14.py
@@ -0,0 +1,13 @@
+'''
+2023.04.05
+신승훈
+5과목 점수 입력받아서 출력
+p117 14번 문제 풀기
+'''
+
+sub1=float(input('과목1:'))
+sub2=float(input('과목2:'))
+sub3=float(input('과목3:'))
+sub4=float(input('과목4:'))
+sub5=float(input('과목5:'))
+
