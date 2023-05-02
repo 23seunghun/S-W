f0ad563f43eba0b0c001d4dcbab798359351d017
승훈 <127714089+23seunghun@users.noreply.github.com>
2023-05-02 13:23:38 +0900

    Add files via upload

---
+++ 0502/0502ex2.py
@@ -0,0 +1,22 @@
+'''
+2023.05.02 신승훈
+
+range함수 순차적 범위의 숫자 생성하는 함수
+
+'''
+
+for i in range(10): #반복 조건
+    print(i,end=' ') #반복조건이 참인 동안 반복
+
+print()
+
+for  k in range(1,10): #반복 조건
+    print(k,end=' ')
+
+print()
+
+for j in range(1,11,2):
+    print(j,end=' ')
+
+for num in range(10,0,-1):
+    print(num,end=' ')
